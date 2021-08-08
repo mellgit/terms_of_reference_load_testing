b79139612291e047a22503bbe5bc04606eda72be
mellgit <daniilhorn8@gmail.com>
2021-08-08 16:16:25 +0300

    add filling the array

--- task1/main.py
+++ task1/main.py
@@ -2,10 +2,22 @@ import sys
 
 
 def main():
-    print(f"{sys.argv[1]=}\n{sys.argv[2]=}") # работа с аргументами
+    # print(f"{sys.argv[1]=}\n{sys.argv[2]=}") # работа с аргументами
+    filling_the_array()
 
 
-# def some_fun():
+def filling_the_array():
+    k = 1
+    ln = []
+    for i in range(2):
+        ln.append([])
+        for j in range(4):
+            ln[i].append(k)
+            k+=1
+    print(ln)
+
+    for i in range(len(ln)):
+        print(ln[i][0])
 
 
 if __name__ == "__main__":
