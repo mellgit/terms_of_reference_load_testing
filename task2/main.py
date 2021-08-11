64654b7daa354303bc992644667c17498397b8b6
mellgit <daniilhorn8@gmail.com>
2021-08-11 23:29:34 +0300

    work on reading rhe file

--- task2/main.py
+++ task2/main.py
@@ -1,16 +1,35 @@
 from sys import argv
 
 def main():
-    search()
+    print(center_and_radius())
+    print()
+    print(coordinates())
 
 
-def search():
+def center_and_radius():
     # print(f'{argv[1]=}\n{argv[2]=}')
-    with open(r"/home/mellgit/github/terms_of_reference_load_testing/task2/f1.txt", "r") as file1:
-        # t = file1.readlines()
-        for line in file1.readlines:
-            print(line)
+    # int(''.join(i for i in just if i.isdigit()))
+    file_name = "file1.txt"
+    ln = []
+    with open(file_name, "r") as file1:
+        for line in file1.read():
+            # ln.append(int(line.strip('\n').replace(' ', '')))
+            # return int(''.join(i for i in line if i.isdigit()))
+            # print(line)
+            ln.append(int(''.join(i for i in line if i.isdigit())))
+    return ln
+
+
+def coordinates():
+    # print(f'{argv[1]=}\n{argv[2]=}')
+    file_name = "file2.txt"
+    ln = []
+    with open(file_name, "r") as file2:
+        for line in file2:
+            ln.append(int(line.strip('\n').replace(' ', '')))
+    return ln
 
 
 if __name__ == "__main__":
     main()
+
